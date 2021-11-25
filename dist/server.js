0fcad2542d5b951c39fe9333ce4d71edc68f12fe
john <johnhowardm@hotmail.com>
2021-11-25 10:28:49 +0000

    initial commit

---
+++ dist/server.js
@@ -0,0 +1,4 @@
+const app = require("./app")
+app.listen(3000, function () {
+    console.log("Server listening on port 3000");
+});
---
+++ src/app.ts
@@ -0,0 +1,57 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const axios = require("axios");
+
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+})
+
+const BASE_URL_MOVIEDB = 'https://api.themoviedb.org/3';
+
+
+
+const API_KEY = process.env.API_KEY_MOVIEDB;
+
+
+interface ResultInterface {
+    data: string,
+    results: string
+}
+
+app.get('/', (req, res) => {
+    res.render("home.html");
+})
+
+app.get('/search', async (req, res) => {
+    console.log("*********In response route*********");
+    console.log("req.query", req.query);
+    const {title} = req.query;
+    const result: ResultInterface = await axios.get(`${BASE_URL_MOVIEDB}/search/movie?api_key=${API_KEY}&query=${title}`);
+    const movies: string = result.data.results;
+    res.render("movielist.html", {movies: movies});
+})
+
+app.get('/moviedetail/:movieid', async (req, res) => {
+    console.log("*********In movie detail route*********");
+    console.log("req.params", req.params);
+    const {movieid} = req.params;
+    const result: ResultInterface = await axios.get(`${BASE_URL_MOVIEDB}/movie/${movieid.substring(1)}?api_key=${API_KEY}`);
+    let data = result.data;
+    res.render("moviedetail.html", {data: data});
+})
+
+
+// app.listen(3000, () => {
+//     console.log("Server listening on port 3000");
+    
+// })
+
+
+module.exports = app;
