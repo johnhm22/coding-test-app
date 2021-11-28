f48098b6080e88ca557466cc7abea1ca41acc242
john <johnhowardm@hotmail.com>
2021-11-28 18:43:10 +0000

    configuring for heroku

--- dist/db.js
+++ dist/db.js
@@ -1,11 +1,10 @@
 var Client = require("pg").Client;
-var DB_URI;
-if (process.env.NODE_ENV === "test") {
-    DB_URI = "postgresql:///mymovies_test";
-}
-else {
-    DB_URI = "postgresql:///mymovies";
-}
+var DB_URI = process.env.DB_URI;
+// if(process.env.NODE_ENV === "test"){
+//     DB_URI = "postgresql:///mymovies_test";
+// } else {
+//     DB_URI = "postgresql:///mymovies";
+// }
 var db = new Client({
     connectionString: DB_URI
 });
--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,12 @@
 const { Client } = require("pg");
 
-let DB_URI;
+let DB_URI = process.env.DB_URI;
 
-if(process.env.NODE_ENV === "test"){
-    DB_URI = "postgresql:///mymovies_test";
-} else {
-    DB_URI = "postgresql:///mymovies";
-}
+// if(process.env.NODE_ENV === "test"){
+//     DB_URI = "postgresql:///mymovies_test";
+// } else {
+//     DB_URI = "postgresql:///mymovies";
+// }
 
 let db = new Client({
     connectionString: DB_URI
