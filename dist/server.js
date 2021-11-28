64d57bd53b5b86c96810739cf0fdb48269280994
john <johnhowardm@hotmail.com>
2021-11-28 19:27:13 +0000

    updating port reference

--- dist/server.js
+++ dist/server.js
@@ -3,5 +3,5 @@ exports.__esModule = true;
 var app = require("./app");
 var PORT = +process.env.PORT;
 app.listen(PORT, function () {
-    console.log("Server listening on http://localhost:".concat(PORT));
+    console.log("Server listening on ".concat(PORT));
 });
--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ const app = require("./app")
 const PORT = +process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 })
