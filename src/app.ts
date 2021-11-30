f118d6cb5baf19b8364548863d97d8e7c1dd0eb8
john <johnhowardm@hotmail.com>
2021-11-30 10:15:24 +0000

    reordering routes

--- src/app.ts
+++ src/app.ts
@@ -39,6 +39,70 @@ app.get('/', (req, res) => {
 })
 
 
+app.get('/users/register', async (req, res) => {
+    try{
+        res.render('register.html');
+    }
+    catch(e){
+        console.log("There was an error when loading register page");
+        res.render('home.html');
+    }
+})
+
+
+app.post('/users/register', async (req, res) => {
+    try{
+        const {username, password} = req.body;
+        const results = await db.query(`INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *`, [username, password]);        
+        req.session.username = username;
+        res.render('home.html');
+    }
+    catch(e){
+        console.log("There was an error when registering");
+        res.render('home.html');
+    }
+})
+
+
+app.get('/users/login', async (req, res) => {
+    try{
+        res.render('login.html');
+    }
+    catch(e){
+        console.log("There was an error when loading login page");
+        res.render('home.html');
+    }
+});
+
+
+app.post('/users/login', async (req, res) => {
+    try{
+        const {username, password} = req.body;
+        const results = await db.query(
+            `SELECT * FROM users password
+            WHERE username = $1`, 
+            [username]);
+        const user = results.rows[0];        
+        if(user.username) {
+            req.session.username = username;
+            res.render('home.html');
+        } else{
+        res.render('home.html');
+        }
+    }
+    catch(e){
+        console.log("There was an error when logging in");
+        res.render('home.html');
+    }
+});
+
+
+app.get('/users/logout', (req, res) => {
+    req.session.username = null;
+    res.render('home.html');
+    });
+
+
 app.get('/faves/:movieid/:title', async (req, res) => {
     let username = req.session.username;
     const {movieid, title} = req.params;
@@ -95,6 +159,7 @@ app.get('/search', async (req, res) => {
     }
 })
 
+
 app.get('/movie/:movieid', async (req, res) => {
     try{
         const {movieid} = req.params;        
@@ -108,65 +173,5 @@ app.get('/movie/:movieid', async (req, res) => {
     }
 })
 
-
-app.get('/users/register', async (req, res) => {
-    try{
-        res.render('register.html');
-    }
-    catch(e){
-        console.log("There was an error when loading register page");
-        res.render('home.html');
-    }
-})
-
-app.post('/users/register', async (req, res) => {
-    try{
-        const {username, password} = req.body;
-        const results = await db.query(`INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *`, [username, password]);        
-        req.session.username = username;
-        res.render('home.html');
-    }
-    catch(e){
-        console.log("There was an error when registering");
-        res.render('home.html');
-    }
-})
-
-app.get('/users/login', async (req, res) => {
-    try{
-        res.render('login.html');
-    }
-    catch(e){
-        console.log("There was an error when loading login page");
-        res.render('home.html');
-    }
-})
-
-app.post('/users/login', async (req, res) => {
-    try{
-        const {username, password} = req.body;
-        const results = await db.query(
-            `SELECT * FROM users password
-            WHERE username = $1`, 
-            [username]);
-        const user = results.rows[0];        
-        if(user.username) {
-            req.session.username = username;
-            res.render('home.html');
-        } else{
-        res.render('home.html');
-        }
-    }
-    catch(e){
-        console.log("There was an error when logging in");
-        res.render('home.html');
-    }
-})
-
-
-app.get('/users/logout', (req, res) => {
-    req.session.username = null;
-    res.render('home.html');
-    })
 
 module.exports = app;
