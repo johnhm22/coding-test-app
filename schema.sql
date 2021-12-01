3bb579ebebde3106a060456437097e1fcbbab39e
john <johnhowardm@hotmail.com>
2021-12-01 09:48:16 +0000

    removing not null from username primary key

--- schema.sql
+++ schema.sql
@@ -6,12 +6,12 @@ CREATE DATABASE "mymovies";
 
 
 CREATE TABLE users (
-username VARCHAR(25) PRIMARY KEY NOT NULL,
+username VARCHAR(25) PRIMARY KEY,
 password VARCHAR(25) NOT NULL,
 created_on TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
                     
 CREATE TABLE faves (movie_id INTEGER PRIMARY KEY,
-movie_title TEXT,
+movie_title TEXT NOT NULL,
 username TEXT NOT NULL REFERENCES users ON DELETE CASCADE
 );
