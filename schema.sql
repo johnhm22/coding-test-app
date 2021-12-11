fc5f51b47393fd9731eb0d90a9ce4f21a38c8cd3
JohnM <johnhowardm@hotmail.com>
2021-12-11 09:30:49 +0000

    adding dual primary key in faves table

--- schema.sql
+++ schema.sql
@@ -11,7 +11,9 @@ password VARCHAR(25) NOT NULL,
 created_on TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
                     
-CREATE TABLE faves (movie_id INTEGER PRIMARY KEY,
+CREATE TABLE faves (
+movie_id INTEGER NOT NULL,
 movie_title TEXT NOT NULL,
-username TEXT NOT NULL REFERENCES users ON DELETE CASCADE
+username TEXT NOT NULL REFERENCES users ON DELETE CASCADE,
+PRIMARY KEY (movie_id, username)
 );
